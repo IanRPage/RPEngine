0dec82dcc0f19401fa748f7b7171d08b6152b1d1
IanRPage <ipragmatic2r@gmail.com>
2025-09-20 14:24:48 -0400

    no longer using smart pointers in `QuadTree`
    i got a performance gain from doing this

--- include/dsa/QuadTree.hpp
+++ include/dsa/QuadTree.hpp
@@ -2,40 +2,26 @@
 #define QUADTREE_H
 
 #include <dsa/AABB.hpp>
-#include <memory>
 #include <vector>
 
 template <typename T>
 class QuadTree {
- private:
-  size_t capacity_;
-  std::vector<T*> data_;
-  AABBf boundary_;
-  bool divided_ = false;
-
-  std::unique_ptr<QuadTree> ul_, ur_, bl_, br_;
-
-  void subdivide() {
-    const float x = boundary_.min.x, y = boundary_.min.y;
-    const float w = 0.5f * boundary_.width(), h = 0.5f * boundary_.height();
-
-    ul_ = std::make_unique<QuadTree>(AABBf({x, y}, {w, h}), capacity_);
-    ur_ = std::make_unique<QuadTree>(AABBf({x + w, y}, {w, h}), capacity_);
-    bl_ = std::make_unique<QuadTree>(AABBf({x, y + h}, {w, h}), capacity_);
-    br_ = std::make_unique<QuadTree>(AABBf({x + w, y + h}, {w, h}), capacity_);
-
-    divided_ = true;
-
-    std::vector<T*> old = std::move(data_);
-    for (T* p : old) {
-      insert(p);
-    }
-  };
-
  public:
-  QuadTree(AABBf bound, size_t cap) : capacity_(cap), boundary_(bound) {
-    data_.reserve(capacity_);
+  QuadTree(AABBf bound, size_t cap)
+      : capacity_(cap),
+        boundary_(bound),
+        ul_(nullptr),
+        ur_(nullptr),
+        bl_(nullptr),
+        br_(nullptr) {
+    data_.reserve(cap);
   };
+  ~QuadTree() {
+    delete ul_;
+    delete ur_;
+    delete bl_;
+    delete br_;
+  }
 
   bool insert(T* p) {
     const Vec2f pos(p->position.x, p->position.y);
@@ -52,10 +38,10 @@ class QuadTree {
       subdivide();
     }
 
-    if (ul_->insert(p)) return true;
-    if (ur_->insert(p)) return true;
-    if (bl_->insert(p)) return true;
-    if (br_->insert(p)) return true;
+    if (ul_ && ul_->insert(p)) return true;
+    if (ur_ && ur_->insert(p)) return true;
+    if (bl_ && bl_->insert(p)) return true;
+    if (br_ && br_->insert(p)) return true;
     return false;
   };
 
@@ -72,6 +58,31 @@ class QuadTree {
       if (ur_) ur_->query(res, qRange);
       if (bl_) bl_->query(res, qRange);
       if (br_) br_->query(res, qRange);
+    }
+  };
+
+ private:
+  size_t capacity_;
+  std::vector<T*> data_;
+  AABBf boundary_;
+  bool divided_ = false;
+
+  QuadTree *ul_, *ur_, *bl_, *br_;
+
+  void subdivide() {
+    const float x = boundary_.min.x, y = boundary_.min.y;
+    const float w = 0.5f * boundary_.width(), h = 0.5f * boundary_.height();
+
+    ul_ = new QuadTree(AABBf({x, y}, {w, h}), capacity_);
+    ur_ = new QuadTree(AABBf({x + w, y}, {w, h}), capacity_);
+    bl_ = new QuadTree(AABBf({x, y + h}, {w, h}), capacity_);
+    br_ = new QuadTree(AABBf({x + w, y + h}, {w, h}), capacity_);
+
+    divided_ = true;
+
+    std::vector<T*> old = std::move(data_);
+    for (T* p : old) {
+      insert(p);
     }
   };
 };
