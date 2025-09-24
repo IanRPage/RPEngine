39bec6b740ab15386ebd8e271a8a62a2e96ad9cd
IanRPage <ipragmatic2r@gmail.com>
2025-09-24 01:42:33 -0400

    main.cpp - added note regarding euler integration

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include <ui/Renderer.hpp>
 
 int main() {
-  // NOTE: values set here in sim constructor will be changed by Renderer
-  // Simulator sim({0.0f, 0.0f}, 5.0f, 0.0f, 0.0f, 0.0f, IntegrationType::Euler, 100000);
+	// NOTE: running sim w/ Euler integration makes some really cool fx
+  // Simulator sim({0.0f, 0.0f}, 2.0f, 0.0f, 0.0f, 0.0f, IntegrationType::Euler, 100000);
   Simulator sim({0.0f, 0.0f}, 2.0f, 0.0f, 0.0f, 0.0, IntegrationType::Verlet, 100000);
 
   Renderer renderer(sim, Renderer::Options{60, "RPEngine"});
