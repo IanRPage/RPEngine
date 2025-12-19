268bdb8461def4ebeff5705ce93edae3d2b0e5f3
IanRPage <ipragmatic2r@gmail.com>
2025-12-19 17:42:20 -0500

    add Enums.hpp to decouple enums from `Simulator`
    i didn't want the enum classes defined in `Simulator`

---
+++ include/core/Enums.hpp
@@ -0,0 +1,9 @@
+#ifndef ENUMS_HPP
+#define ENUMS_HPP
+
+enum class IntegrationType { Euler, Verlet };
+enum class BroadphaseType { Naive, Qtree, SpatialGrid };
+enum class ForceType { None, Radial };
+enum class SpawnType { None, Manual, Random, Stream, Max };
+
+#endif
