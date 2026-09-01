c6f3b5c3bd6c520f79eac3ba8b9aa50391a15597
cHuNgStEn <ipragmatic2r@gmail.com>
2026-09-01 18:49:59 -0400

    add quaternion integration and nlerp for rotation

---
+++ include/math/Constraints2D.hpp
@@ -0,0 +1,9 @@
+#ifndef RPENGINE_MATH_CONSTRAINTS2D_HPP
+#define RPENGINE_MATH_CONSTRAINTS2D_HPP
+
+#include <math/Types.hpp>
+
+void apply2DConstraint(Vec3f& position, Vec3f& linearVelocity,
+                        Vec3f& angularVelocity) noexcept;
+
+#endif
